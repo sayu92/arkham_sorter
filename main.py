c856b727c82e11be3821767fd2e14a3a131dda80
Sayu92 <lnicolas.douce@gmail.com>
2024-01-14 02:21:57 +0100

    Previous bug is solved, image is successfully modified and dislpays the roi, todo: - bug when the camera detects some text that leads to nowhere (confirm this), the camera is really laggy when performing easyocr

--- main.py
+++ main.py
@@ -25,14 +25,14 @@ def update(source : str, ratio : float = 1.4225, reduc : float = 0.8, processing
         centroid = ( h//2, w//2)
         roi_pt1 = [centroid[1] - w_new//2, centroid[0] - h_new//2]
         roi_pt2 = [centroid[1]+ w_new//2, centroid[0]+h_new//2]
-        #cv.rectangle(frame, roi_pt1, roi_pt2, thickness=5, color=(255,0,0))
+        cv.rectangle(frame, roi_pt1, roi_pt2, thickness=5, color=(255,0,0))
         #roi draw n extract
         roi_capture = frame[roi_pt1[1]:roi_pt2[1],roi_pt1[0]:roi_pt2[0]]
         img_ratio = processing_resolution[1]/roi_capture.shape[1]
         roi_capture = cv.resize(roi_capture, None, fx= img_ratio, fy= img_ratio)  
         roi_capture = cv.cvtColor(roi_capture, cv.COLOR_RGB2GRAY)              
     
-    else:
+    else: #TODO this section will led to bug, only valuable for debug purpose, 
         roi_pt1 = [0, 0]
         roi_pt2 = [frame.shape[1]-1, frame.shape[0]-1]
         roi_capture = frame #TODO Pas très efficace de copier un tableau
@@ -41,39 +41,39 @@ def update(source : str, ratio : float = 1.4225, reduc : float = 0.8, processing
     #draw the boxname on the frame
     posx_namebox1 = round(rel_pos_centroid_namebox1[0]*roi_capture.shape[1])
     posy_namebox1 = round(rel_pos_centroid_namebox1[1]*roi_capture.shape[0])
-    roi_boxname1_centroid = [posx_namebox1 + roi_pt1[0], posy_namebox1 + roi_pt1[1]]
-    roi_boxname1_pt1 = [roi_boxname1_centroid[0] - round(size_ratio_boxname1[0]*frame.shape[1])//2, roi_boxname1_centroid[1] - round(size_ratio_boxname1[1]*frame.shape[0])//2]
-    roi_boxname1_pt2 = [roi_boxname1_centroid[0] + round(size_ratio_boxname1[0]*frame.shape[1])//2, roi_boxname1_centroid[1] +round(size_ratio_boxname1[1]*frame.shape[0])//2]
-    roi_boxname1 = frame[roi_boxname1_pt1[1]: roi_boxname1_pt2[1], roi_boxname1_pt1[0]:roi_boxname1_pt2[0]]
-    roi_boxname1 = cv.cvtColor(roi_boxname1, cv.COLOR_RGB2GRAY) # TODO Changer pour ne pas effectuer une deuxième fois la conversion(Il faut slicer depuis roi_capture)
+    roi_boxname1_centroid = [posx_namebox1, posy_namebox1]
+    roi_boxname1_pt1 = [roi_boxname1_centroid[0] - round(size_ratio_boxname1[0]*roi_capture.shape[1])//2, roi_boxname1_centroid[1] - round(size_ratio_boxname1[1]*roi_capture.shape[0])//2]
+    roi_boxname1_pt2 = [roi_boxname1_centroid[0] + round(size_ratio_boxname1[0]*roi_capture.shape[1])//2, roi_boxname1_centroid[1] +round(size_ratio_boxname1[1]*roi_capture.shape[0])//2]
+    roi_boxname1 = roi_capture[roi_boxname1_pt1[1]: roi_boxname1_pt2[1], roi_boxname1_pt1[0]:roi_boxname1_pt2[0]]
+    #roi_boxname1 = cv.cvtColor(roi_boxname1, cv.COLOR_RGB2GRAY) # TODO Changer pour ne pas effectuer une deuxième fois la conversion(Il faut slicer depuis roi_capture)
     
-    cv.rectangle(frame,roi_boxname1_pt1, roi_boxname1_pt2, thickness=2, color=(0,255,0) )
+    cv.rectangle(roi_capture,roi_boxname1_pt1, roi_boxname1_pt2, thickness=2, color=255 )
     
     posx_namebox2 = round(rel_pos_centroid_namebox2[0]*roi_capture.shape[1])
     posy_namebox2 = round(rel_pos_centroid_namebox2[1]*roi_capture.shape[0])
-    roi_boxname2_centroid = [posx_namebox2 + roi_pt1[0], posy_namebox2 + roi_pt1[1]]
-    roi_boxname2_pt1 = [roi_boxname2_centroid[0] - round(size_ratio_boxname2[0]*frame.shape[1])//2, roi_boxname2_centroid[1] - round(size_ratio_boxname2[1]*frame.shape[0])//2]
-    roi_boxname2_pt2 = [roi_boxname2_centroid[0] + round(size_ratio_boxname2[0]*frame.shape[1])//2, roi_boxname2_centroid[1] +round(size_ratio_boxname2[1]*frame.shape[0])//2]
-    roi_boxname2 = frame[roi_boxname2_pt1[1]: roi_boxname2_pt2[1], roi_boxname2_pt1[0]:roi_boxname2_pt2[0]]
-    roi_boxname2 = cv.cvtColor(roi_boxname2, cv.COLOR_RGB2GRAY) # TODO Changer pour ne pas effectuer une deuxième fois la conversion(Il faut slicer depuis roi_capture)
+    roi_boxname2_centroid = [posx_namebox2, posy_namebox2]
+    roi_boxname2_pt1 = [roi_boxname2_centroid[0] - round(size_ratio_boxname2[0]*roi_capture.shape[1])//2, roi_boxname2_centroid[1] - round(size_ratio_boxname2[1]*roi_capture.shape[0])//2]
+    roi_boxname2_pt2 = [roi_boxname2_centroid[0] + round(size_ratio_boxname2[0]*roi_capture.shape[1])//2, roi_boxname2_centroid[1] +round(size_ratio_boxname2[1]*roi_capture.shape[0])//2]
+    roi_boxname2 = roi_capture[roi_boxname2_pt1[1]: roi_boxname2_pt2[1], roi_boxname2_pt1[0]:roi_boxname2_pt2[0]]
+    #roi_boxname2 = cv.cvtColor(roi_boxname2, cv.COLOR_RGB2GRAY) # TODO Changer pour ne pas effectuer une deuxième fois la conversion(Il faut slicer depuis roi_capture)
     
-    #cv.rectangle(frame,roi_boxname2_pt1, roi_boxname2_pt2, thickness=2, color=(0,255,0) )
+    cv.rectangle(roi_capture,roi_boxname2_pt1, roi_boxname2_pt2, thickness=2, color=255 )
     return frame, roi_boxname1, roi_boxname2
 
 if __name__ == '__main__':
     
     while True :
-        roi_capture, boxname1, boxname2 = update(path_ip, reduc=0.8, resize=True)
+        frame, boxname1, boxname2 = update(path_ip, reduc=0.8, resize=True)
     
-        for boxname in [boxname1, boxname2]: 
-            result = ocr_card_name(boxname)
+        # for boxname in [boxname1, boxname2]: 
+        #     result = ocr_card_name(boxname)
         
-            if len(result) == 1: # Any other answer is a false positive
-                position = trouver_position(result[0][1])
-                cv.putText(roi_capture, f"Page: {result[0]}, Position: {result[1]}", (300,300))
+        #     if len(result) == 1: # Any other answer is a false positive
+        #         position = trouver_position(result[0][1])
+        #         cv.putText(frame, f"Page: {result[0]}, Position: {result[1]}", (300,300))
             
-            cv.imshow("camera", roi_capture)
-            k = cv.waitKey(10) & 0XFF
+        cv.imshow("camera", frame)
+        k = cv.waitKey(10) & 0XFF
     
     cv.destroyAllWindows()
     
